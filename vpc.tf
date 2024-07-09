6851d7b5fbc887305f62f223cb768e13839b956c
unknown <teamstac350@gmail.com>
2024-07-09 17:26:13 +0800

    second

--- vpc.tf
+++ vpc.tf
@@ -44,3 +44,50 @@ resource "aws_route_tables_association" "PriRTasso"{
     subnet_id = "aws.subnet.public-subnet.id"
     route_table_id = "aws.route.table.Publicrt.id"
 }
+
+# Security Group
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "main-sg"
+  }
+}
+
+# EC2 Instance in Public Subnet
+resource "aws_instance" "public_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public-server.id
+  security_groups = [aws_security_group.main.name]
+
+  tags = {
+    Name = "public-instance"
+  }
+}
+
+# EC2 Instance in Private Subnet
+resource "aws_instance" "private_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private-subnet.id
+  security_groups = [aws_security_group.main.name]
+
+  tags = {
+    Name = "private-instance"
+  }
+}
